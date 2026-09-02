6dc6d914d88885bfedb8b4b0ba474bbe237f86e6
kmanika982-prog <kmanika982@gmail.com>
2026-09-02 10:45:59 +0545

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = "127.0.0.1";
-$user = "root";
-$password = "";
-$database = "online_fee_system";
+$host = "YOUR_HOST";
+$user = "YOUR_USERNAME";
+$password = "YOUR_PASSWORD";
+$database = "if0_42787088_online_fee_system";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8mb4");
 
 ?>
