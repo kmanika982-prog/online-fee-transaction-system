5af3fb3bc0e6405a6decb5bf21dd208e55d84fce
kmanika982-prog <kmanika982@gmail.com>
2026-09-05 18:26:39 +0545

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,16 +1,14 @@
 <?php
 
-$host = "sql203.infinityfree.com";
-$user = "if0_42787088";
-$password = "abc12345";
-$database = "if0_42787088_online_fee_system";
+$host = "127.0.0.1";
+$user = "root";
+$password = "manika";
+$database = "online_fee_system";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
-
-mysqli_set_charset($conn, "utf8mb4");
 
 ?>
